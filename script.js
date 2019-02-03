5fd421b37a83614f1885cc7adf10215dcadb4a94
tochwat <tochwat@gmail.com>
2019-02-03 11:01:48 -0800

    working v1

--- script.js
+++ script.js
@@ -50,12 +50,44 @@ var submit = function(event) {
   var firstTrain = $("#first-train-input").val().trim();
   var frequency = $("#frequency-input").val().trim();
 
+  //----------------------------------------Adding Moment.JS code to submit function----------------------------//
+
+  //First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+  console.log(firstTrainConverted);
+
+  // Current Time
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % frequency;
+  console.log(tRemainder);
+
+  // Minute Until Train
+  var minutesAway = frequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + minutesAway);
+
+  // Next Train
+  var nextTrain = moment().add(minutesAway, "minutes");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+  var nextArrival = moment(nextTrain).format("hh:mm");
+
+
+  //----------------------------------------End Moment.js code --------------------------------------------------//
+
 
   console.log({
       name: name,
       destination: destination,
       frequency: frequency,
-      firstTrain: firstTrain
+      firstTrain: firstTrain,
+      nextArrival: nextArrival,
+      minutesAway: minutesAway
     })
 
   // Change what is saved in firebase
@@ -63,7 +95,9 @@ var submit = function(event) {
     name: name,
     destination: destination,
     frequency: frequency,
-    firstTrain: firstTrain
+    firstTrain: firstTrain,
+    nextArrival: nextArrival,
+    minutesAway: minutesAway
   });
   $(".input-fields").val("")
 }
@@ -88,6 +122,8 @@ connectedRef.on("child_added", function (snapshot) {
   console.log(snapshot.val().destination);
   console.log(snapshot.val().frequency);
   console.log(snapshot.val().firstTrain);
+  console.log(snapshot.val().nextArrival);
+  console.log(snapshot.val().minutesAway);
 
   // dynamically create new table rows
 
@@ -95,9 +131,9 @@ connectedRef.on("child_added", function (snapshot) {
   $("#name-display").text(snapshot.val().name);
   $("#destination-display").text(snapshot.val().destination);
   $("#frequency-display").text(snapshot.val().frequency);
-  // $("#next-arrival-display").text(snapshot.val().nextArrival);
-  // $("#minutes-away-display").text(snapshot.val().minutesAway);
-  
+  $("#next-arrival-display").text(snapshot.val().nextArrival);
+  $("#minutes-away-display").text(snapshot.val().minutesAway);
+
   // Handle the errors
 }, function (errorObject) {
   console.log("Errors handled: " + errorObject.code);
